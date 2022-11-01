42bec4a6edf97b70ce41aa1cbf3ed70b29489187
leemaria609@gmail.com <leemaria609@gmail.com>
2022-11-01 13:43:40 +0100

    new branch

---
+++ scripts/travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+EXPECTED_LOG_DIR=$1
+
+# Launch QEMU test
+screen -L -dmS qemu ./scripts/run-qemu.sh
+sleep 10
+./scripts/test-qemu.sh 2>&1 | grep -v "Warning: Permanently added" | tee output.log
+
+diff output.log $EXPECTED_LOG_DIR/test-qemu.expected.log
+if [ $? -eq 0 ]
+then
+  echo "[PASS] output.log matches with the expected output"
+  exit 0
+else
+  echo "[FAIL] output.log does not match with the expected output"
+  exit 1
+fi
